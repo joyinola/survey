af7cb9dc08fbcbc3a867ba12ee0fc3153a8307ce
joyinola <damilolaadetoro1@gmail.com>
2022-05-22 20:20:04 +0100

    debug true

--- survey/admin.py
+++ survey/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import *
 from import_export.admin import ImportExportModelAdmin
-from .resources import VoteResource
+from .resources import VoteResource,HeadlineResource
 # Register your models here.
 class UserAdmin(ImportExportModelAdmin,admin.ModelAdmin):
 	pass
@@ -11,5 +11,5 @@ class VoteAdmin(ImportExportModelAdmin,admin.ModelAdmin):
 	resource_class=VoteResource
 admin.site.register(Vote,VoteAdmin)
 class VoteAdmin(ImportExportModelAdmin,admin.ModelAdmin):
-	resource_class=HeadlinesResource
+	resource_class=HeadlineResource
 admin.site.register(HeadLines)
