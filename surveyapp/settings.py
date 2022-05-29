60215e18cfb6ce94973cd46201c003e172fb2918
joyinola <damilolaadetoro1@gmail.com>
2022-05-29 20:35:21 +0100

    'mobile-btn-style'

--- surveyapp/settings.py
+++ surveyapp/settings.py
@@ -86,8 +86,8 @@ DATABASES = {
     'PASSWORD':config('PASSWORD'),
     'HOST':'ec2-54-165-184-219.compute-1.amazonaws.com',
     'PORT':5432,
-    'ENGINE': 'django.db.backends.sqlite3',
-    'NAME': BASE_DIR / 'db.sqlite3',
+    # 'ENGINE': 'django.db.backends.sqlite3',
+    # 'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
